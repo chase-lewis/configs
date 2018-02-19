64532da0ff6a19131c1aab888c691c1911b3efe8
chase-lewis <chase.lewis.sc@gmail.com>
2018-02-19 10:21:07 -0500

    Initial fedora version

---
+++ lockscreen/lock.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#4b5162ee'  # clear ish
+D='#bbbfcaff'  # default
+T='#bbbfcaff'  # text
+W='#c35c66ee'  # typing
+V='#5294e2ee'  # verifying
+
+/home/octal/.files/lockscreen/i3lock \
+--insidevercolor=$C   \
+--ringvercolor=$V     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$C      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--verifcolor=$T        \
+--wrongcolor=$T        \
+--timecolor=$T        \
+--datecolor=$T        \
+--layoutcolor=$T      \
+--keyhlcolor=$W       \
+--bshlcolor=$W        \
+\
+--screen 1            \
+--blur 1              \
+--clock               \
+--indicator           \
+--timestr="%H:%M:%S"  \
+--datestr="%m-%d-%Y" \
+--keylayout 2         \
+
+# --veriftext="Drinking verification can..."
+# --wrongtext="Nope!"
+# --textsize=20
+# --modsize=10
+# --timefont=comic-sans
+# --datefont=monofur
+# etc
